d18ed891771f612342a17c05b7e32eec59738ce3
MilosVu <vujic.milos1998@gmail.com>
2022-06-24 17:29:21 +0200

    Dodate slike trenera

--- src/components/Trener/TrenerCardComponent.jsx
+++ src/components/Trener/TrenerCardComponent.jsx
@@ -13,7 +13,7 @@ class TrenerCardComponent extends Component {
 
                     <div class="coachImgContainer">
                         <div class="coachImg">
-                            <img width="500" height="600" src="https://onefit.rs/wp-content/uploads/2021/07/milos-milosev-personalni-trener.jpg" class="attachment-full size-full wp-post-image" alt="Miloš Milošev personalni trener" srcset="https://onefit.rs/wp-content/uploads/2021/07/milos-milosev-personalni-trener.jpg 500w, https://onefit.rs/wp-content/uploads/2021/07/milos-milosev-personalni-trener-250x300.jpg 250w" sizes="(max-width: 500px) 100vw, 500px" />												</div>
+                            <img width="500" height="600" src="http://localhost:3000/blank-profile-picture-973460_640.png" class="attachment-full size-full wp-post-image" srcset="http://localhost:3000/blank-profile-picture-973460_640.png" sizes="(max-width: 500px) 100vw, 500px" />												</div>
                     </div>
                     <div class="coachContentContainer">
                         <h4 class="coachName mb-3 text-center">{this.state.trener.ime}</h4>
